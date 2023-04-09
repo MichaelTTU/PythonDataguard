0de285bbbdd539165383f6d7097c5e72cf965a02
MichaelTTU <127047569+MichaelTTU@users.noreply.github.com>
2023-04-09 17:16:12 -0500

    Create multithread_socket_server_test.py

---
+++ multithread_socket_server_test.py
@@ -0,0 +1,65 @@
+
+# import socket programming library
+import socket
+ 
+# import thread module
+# from _thread import *
+import threading
+ 
+print_lock = threading.Lock()
+ 
+# thread function
+def threaded(test_message):
+    
+    host = "10.171.227.118"
+    port = 12345
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    print("socket binded to port", port)
+ 
+    # put the socket into listening mode
+    s.listen(5)
+    print("socket is listening")
+    
+    c, addr = s.accept()
+    print_lock.acquire()
+    print('Connected to :', addr[0], ':', addr[1])
+
+    while True:
+ 
+        # data received from client
+        data = c.recv(1024)
+        if not data:
+            print('Bye')
+             
+            # lock released on exit
+            print_lock.release()
+            break
+ 
+        # reverse the given string from client
+        data = data[::-1]
+ 
+        # send back reversed string to client
+        #c.send(data)
+        c.send(test_message.encode('ascii'))
+ 
+    # connection closed
+    c.close()
+    s.close()
+ 
+ 
+def Main():
+    test_message = "poop"
+    host = "10.171.227.118"
+ 
+    # reserve a port on your computer
+    # in our case it is 12345 but it
+    # can be anything
+    port = 12345
+    t1 = threading.Thread(target=threaded, args=(test_message,))
+    
+ 
+ 
+if __name__ == '__main__':
+    Main()
